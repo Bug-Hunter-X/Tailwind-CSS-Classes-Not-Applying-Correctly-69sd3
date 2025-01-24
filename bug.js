bec1953e611f5859550330dd391ff3086b96bc1b
BugHunter-X <askzenai@gmail.com>
2025-01-24 02:46:06 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,8 @@
+```javascript
+// This is the buggy code in my component.
+<div className="bg-gray-200 p-4">
+  <h1 className="text-2xl font-bold">My Component</h1>
+  <p className="text-gray-700">This is some text.</p>
+  <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Click me</button>
+</div>
+```
